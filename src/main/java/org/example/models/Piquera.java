dd1f7b2ad27e16cb2368ebe4318830fd2797a9f2
Elvis Hernández <122816168+ElvisHdez@users.noreply.github.com>
2024-04-25 00:42:23 -0400

    Update Piquera.java
    static y comentarios

--- src/main/java/org/example/models/Piquera.java
+++ src/main/java/org/example/models/Piquera.java
@@ -4,28 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Piquera {
-    List<Taxi> taxis = new ArrayList<>();
-    List<Solicitud> solicitudes = new ArrayList<>();
-    List<SolicitudEnEjecucion> solicitudesEnEjecucion = new ArrayList<>();
-    public void asignarSolicitudes(){
+    static List<Taxi> taxis = new ArrayList<>();
+    static List<Solicitud> solicitudes = new ArrayList<>();
+    static List<SolicitudEnEjecucion> solicitudesEnEjecucion = new ArrayList<>();
+    public static void asignarSolicitudes(){
         for (Solicitud solicitud : solicitudes) {
             for (Taxi taxi : taxis) {
                 if (taxi.getCapacidad()>=solicitud.getCantPersonas()
                 && taxi.getCombustible()*5>= solicitud.getCantKm()){
                     taxis.remove(taxi);
+                    solicitudes.remove(solicitud);
                     solicitudesEnEjecucion.add(new SolicitudEnEjecucion(solicitud,taxi));
+                    // actualizar base de datos, crear Servicio En ejecucion
+                    // eliminar las solicitudes pendientes
+                    // y actualizar el estado de los taxis de disponibles a ocupados
                     break;
                 }
             }
         }
     }
-    public void procesarSolicitudInmediata(Solicitud solicitud){
+    public static void procesarSolicitudInmediata(Solicitud solicitud){
         for (Taxi taxi : taxis) {
             if (taxi.getCapacidad()>=solicitud.getCantPersonas()
                     && taxi.getCombustible()*5>= solicitud.getCantKm()){
                 taxis.remove(taxi);
                 solicitudes.remove(solicitud);
                 solicitudesEnEjecucion.add(new SolicitudEnEjecucion(solicitud,taxi));
+                // actualizar base de datos, crear Servicio En ejecucion
+                // eliminar las solicitudes pendientes
+                // y actualizar el estado de los taxis de disponibles a ocupados
                 break;
             }
         }
